616cd2c2bf00955d45d0531439672ea98b7bcba7
JKINGKONG <1339064067@qq.com>
2020-04-23 15:05:15 +0800

    添加注释

--- backbones/resnet.py
+++ backbones/resnet.py
@@ -503,7 +503,7 @@ class ResNet(nn.Module):
             x = res_layer(x)
             if i in self.out_indices:
                 outs.append(x)
-        print("---------------------------------------resnet.py forward--------------------------------------")
+        # print("---------------------------------------resnet.py forward--------------------------------------")
         return tuple(outs)
 
     def train(self, mode=True):
